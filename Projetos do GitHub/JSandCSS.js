85e8daf25121c7993379921c0da8c1ecde2c4ae7
nogkay <anunciacaomatheus210@gmail.com>
2026-08-07 14:37:13 -0300

    removendo um folder

--- Projetos do GitHub/JSandCSS.js
+++
@@ -1,8 +0,0 @@
-let painel = document.querySelector("#painel");
-let peso = Number(window.prompt("Digite o peso do atleta"));
-
-if (peso > 90) {
-    painel.classList.add("erro");
-} else {
-    painel.classList.remove("erro");
-}
